09ac28934fd1b9cae1c5ca261495ef2cffb90fba
coolperson111 <chakalguk@gmail.com>
2023-06-12 15:41:24 +0530

    refactoring

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "help", "java", "python", "c", "lua" },
+  ensure_installed = { "java", "python", "c", "lua" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,

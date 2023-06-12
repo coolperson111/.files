e4cbb43cc82e615127d32bf8e834d63e50154510
coolperson111 <chakalguk@gmail.com>
2023-06-12 16:01:23 +0530

    changing file structure

--- .config/nvim/lua/malhar/init.lua
+++ .config/nvim/lua/malhar/init.lua
@@ -1,3 +1,4 @@
 require("malhar.remap")
 require("malhar.packer")
 require("malhar.set")
+require("malhar.plugin_config")
---
+++ .config/nvim/lua/malhar/plugin_config/init.lua
@@ -0,0 +1,4 @@
+require("malhar.plugin_config.colors")
+require("malhar.plugin_config.telescope")
+require("malhar.plugin_config.treesitter")
+require("malhar.plugin_config.lsp")

f59d73b701cfcba5523dc89000e86227d91ae20a
coolperson111 <chakalguk@gmail.com>
2023-06-12 15:29:27 +0530

    match

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,15 +1,6 @@
--- dracula.setup({
-	-- show_end_of_buffer = true, -- default false
-	--transparent_bg = true, -- default false
-	--lualine_bg_color = "#44475a", -- default nil
---})
-
 require('onedark').setup {
 	transparent = true,
 	style = 'warmer',
 }
 
-
-
 require('onedark').load()
----vim.cmd[[colorscheme dracula]]

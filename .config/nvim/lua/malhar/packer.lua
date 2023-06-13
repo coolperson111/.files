0e0f214e805a0cb6e2d15a56a8589b87e7347613
coolperson111 <chakalguk@gmail.com>
2023-06-13 20:19:27 +0530

    autocompletions

--- .config/nvim/lua/malhar/packer.lua
+++ .config/nvim/lua/malhar/packer.lua
@@ -25,10 +25,19 @@ return require('packer').startup(function(use)
 
     use 'nvim-lualine/lualine.nvim'
 
+    -- lsp
     use {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
         "neovim/nvim-lspconfig",
     }
+
+    -- autocomplete
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer' -- lsp autocomplete
+    use 'hrsh7th/cmp-path' -- lsp autocomplete
+    use 'L3MON4D3/LuaSnip' -- snippet engine
+    use 'saadparwaiz1/cmp_luasnip'
 
 end)

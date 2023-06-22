47a8d2191a08c87fd569a017e403bccf82f3a661
coolperson111 <chakalguk@gmail.com>
2023-06-22 23:50:15 +0530

    setup update

--- setup.sh
+++ setup.sh
@@ -14,7 +14,4 @@ sudo apt install neovim
 sudo apt install ripgrep fd-find openssh
 sudo apt install neofetch htop
 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-curl -sS https://starship.rs/install.sh | sh

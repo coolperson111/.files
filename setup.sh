5ff435054902bf4d9a5b6843a0bada9782989a6d
coolperson111 <chakalguk@gmail.com>
2023-06-23 17:00:23 +0530

    setup.sh

--- setup.sh
+++ setup.sh
@@ -14,4 +14,20 @@ sudo apt install neovim
 sudo apt install ripgrep fd-find openssh
 sudo apt install neofetch htop
 
+# nvim config
+ln ~/Projects/.files/.config/nvim/init.lua ~/.config/nvim/init.lua
+ln ~/Projects/.files/.config/nvim/lua/malhar/*.lua ~/.config/nvim/lua/malhar/./
+ln ~/Projects/.files/.config/nvim/lua/malhar/plugin_config/*.lua ~/.config/nvim/lua/malhar/plugin_config/./
+
+# zsh config
+ln ~/Projects/.files/.config/.profile ~/.config/.profile
+ln ~/Projects/.files/.aliasrc ~/./
+ln ~/Projects/.files/.zshrc ~/./
+ln ~/Projects/.files/.zprofile ~/./
+
+# kitty config
+ln ~/Projects/.files/.config/kitty/* ~/.config/kitty/./
+
+
+
 

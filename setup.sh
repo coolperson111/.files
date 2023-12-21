9f2aca1cdaeab8ce6a1d5de0e17c12832ea386a5
coolperson111 <chakalguk@gmail.com>
2023-12-21 14:04:52 +0530

    improved setup

--- setup.sh
+++ setup.sh
@@ -14,15 +14,34 @@ sudo apt install neovim
 sudo apt install ripgrep fd-find openssh
 sudo apt install neofetch htop
 
-# nvim config
-ln ~/Projects/.files/.config/nvim/init.lua ~/.config/nvim/init.lua
-ln ~/Projects/.files/.config/nvim/lua/malhar/*.lua ~/.config/nvim/lua/malhar/./
-ln ~/Projects/.files/.config/nvim/lua/malhar/plugin_config/*.lua ~/.config/nvim/lua/malhar/plugin_config/./
+if [ -d ~/.config/nvim ]; then
+  echo "nvim config already exists, renaming to nvim_old"
+  mv ~/.config/nvim ~/.config/nvim_old
+fi
+cp -r ~/Projects/.files/.config/nvim/ ~/.config/nvim/
 
-# zsh config
+if [ -f ~/.config/.profile ]; then
+  echo ".profile already exists, renaming to .profile_old"
+  mv ~/.config/.profile ~/.config/.profile_old
+fi
 ln ~/Projects/.files/.config/.profile ~/.config/.profile
+
+if [ -f ~/.aliasrc ]; then
+  echo ".aliasrc already exists, renaming to .aliasrc_old"
+  mv ~/.aliasrc ~/.aliasrc_old
+fi
 ln ~/Projects/.files/.aliasrc ~/./
+
+if [ -f ~/.zshrc ]; then
+  echo ".zshrc already exists, renaming to .zshrc_old"
+  mv ~/.zshrc ~/.zshrc_old
+fi
 ln ~/Projects/.files/.zshrc ~/./
+
+if [ -f ~/.zprofile ]; then
+  echo ".zprofile already exists, renaming to .zprofile_old"
+  mv ~/.zprofile ~/.zprofile_old
+fi
 ln ~/Projects/.files/.zprofile ~/./
 
 # kitty config
